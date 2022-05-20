fabfaa21f6e1ccba58bd8eb3ea8597a72a56f108
ritesh chaudhary <riteshchaudhary587@gmail.com>
2022-05-20 15:19:06 +0530

    added features

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,62 +1,65 @@
-/* eslint-disable @next/next/no-img-element */
-import { BellIcon, SearchIcon } from "@heroicons/react/solid";
-import Link from "next/link";
-import { useState, useEffect } from "react";
+import Image from 'next/image'
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import useAuth from '../hooks/useAuth'
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
+import BasicMenu from './BasicMenu'
 
-const header = () => {
-  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+function Header() {
+  const [isScrolled, setIsScrolled] = useState(false)
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 0) {
-        setIsScrolled(true);
+        setIsScrolled(true)
       } else {
-        setIsScrolled(false);
+        setIsScrolled(false)
       }
-    };
+    }
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll)
 
-    // clean up the function
     return () => {
-      window.addEventListener("scroll", handleScroll);
-    };
-  }, []);
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <header className={`${isScrolled && "bg-[#141414]"}`}>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://rb.gy/ulxxee"
-          alt="logo"
           width={100}
           height={100}
           className="cursor-pointer object-contain"
         />
+
+        <BasicMenu />
+
         <ul className="hidden space-x-4 md:flex">
           <li className="headerLink cursor-default font-semibold text-white hover:text-white">
             Home
           </li>
-          <li className="headerLink">Movies</li>
           <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
           <li className="headerLink">New & Popular</li>
           <li className="headerLink">My List</li>
         </ul>
       </div>
-      <div className="flex items-center space-x-2 text-sm font-light">
-        <SearchIcon className="hidden h-5 w-5 sm:inline" />
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
         <p className="hidden lg:inline">Kids</p>
-        <BellIcon className="h-5 w-5" />
-
+        <BellIcon className="h-6 w-6" />
         <Link href="/account">
           <img
             src="https://rb.gy/g1pwyx"
+            alt=""
             className="cursor-pointer rounded"
-            alt="profile image"
           />
         </Link>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default header;
+export default Header
